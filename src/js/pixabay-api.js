29f1ba6eea8a3492781d219414af41e42ac1caf2
Eduard <edik.moskalets@gmail.com>
2024-06-16 20:11:43 +0300

    upd

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,20 @@
+export function getPhotos() {
+    const BASE_URL = 'https://pixabay.com';
+    const API_KEY = '44405907-01ac5a46b548f68b2a274235a'
+
+    const params = {
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        key: API_KEY,   
+    }
+
+    const options = ner URLSearchParams(params);
+    return fetch(`${BASE_URL}?${options}`).then(res => {
+        if (!res.ok) {
+            throw new Error('Error!');
+        }
+        return res.json();
+    });
+}
